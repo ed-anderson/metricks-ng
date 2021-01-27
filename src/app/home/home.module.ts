4d8f2feb2eae1143c638a408ebcd36ef3fd7854a
Edward Anderson <edwardchristopheranderson@gmail.com>
2021-01-27 18:45:31 +0000

    Added calculator component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CalculatorComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
